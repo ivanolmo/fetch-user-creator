50e00a33817f017cbe779efffe79f6085e94319d
Ivan Olmo <ivan.olmo@gmail.com>
2022-10-26 15:38:02 -0500

    refactor: replace boilerplate with app code and my links

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Toaster } from 'react-hot-toast';
+
+import Form from './Form';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header>
+        <h1>Fetch Rewards</h1>
+        <p>This form is built with:</p>
+        <ul>
+          <li>React</li>
+          <li>TypeScript</li>
+          <li>React Hook Form</li>
+          <li>react-hot-toast</li>
+          <li>react-spinners</li>
+        </ul>
+        <div className='links__container'>
+          <span>My links:</span>
+          <a
+            href='https://github.com/ivanolmo'
+            target='_blank'
+            rel='noreferrer'
+          >
+            GitHub
+          </a>
+          <a
+            href='https://linkedin.com/in/ivan-olmo'
+            target='_blank'
+            rel='noreferrer'
+          >
+            LinkedIn
+          </a>
+          <a href='https://ivanolmo.io' target='_blank' rel='noreferrer'>
+            Portfolio
+          </a>
+        </div>
       </header>
+      <Form />
+      <Toaster />
     </div>
   );
 }
